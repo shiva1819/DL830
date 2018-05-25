4c5a457679cf88acc2defb4aac27fdabf8d46d41
Shiva Prasad <as.prasad50@gmail.com>
2018-05-26 00:54:16 +0530

    js app

---
+++ js/app/js/main.js
@@ -0,0 +1,23 @@
+/*var closer = document.getElementById('closer');
+var contentR = document.getElementById('contentR');
+
+closer.style.transform = 'rotateY(0deg)'
+
+closer.onclick = function(){
+	// alert();
+
+
+	if(closer.style.transform == 'rotateY(0deg)'){
+		closer.style.transform = 'rotateY(180deg)'
+		// this.style.left = "-30px";
+		contentR.style.right = "-405px";
+		contentL.style.width = "100%";
+	}else{
+		closer.style.transform = 'rotateY(0deg)'
+		// this.style.left = "-30px";
+		contentR.style.right = "0px";
+		contentL.style.width = "70%";
+	}
+	
+};
+*/
